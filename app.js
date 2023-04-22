be65dde567547e52c43fce9977c4cf2f3075b4ab
mirzaulayya76 <mirzaulayya@gmail.com>
2023-04-22 11:20:13 +0700

    nambahin api ch8 dan backend server

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const apiRouter = require('./server/routes')
-const errorHandler = require('./server/middlewares/errorHandler')
-const PORT = process.env.PORT || 4000
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const apiRouter = require('./server/routes');
+const errorHandler = require('./server/middlewares/errorHandler');
+const PORT = process.env.PORT || 4000;
+const swaggerUI = require('swagger-ui-express');
+const swaggerJSON = require('./chapter8-api.json');
 
 // middlewares
-app.use(cors())
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(errorHandler)
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(errorHandler);
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 
 /**
  * @Routes /api
  * entrypoint for all API routes
  */
-app.use("/api", apiRouter)
+app.use('/api', apiRouter);
+app.get('/login', (req, res) => {
+  res.send('halaman login');
+});
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`)
-})
+  console.log(`Listening on http://localhost:${PORT}`);
+});
